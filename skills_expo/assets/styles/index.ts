e457bc78c6242fc8c053214e1e61a2094b535fa6
Pranav Anil <pranavanil@Pranavs-MacBook-Pro-3.local>
2025-04-24 18:31:28 +0530

    Your message about the change

--- skills_expo/assets/styles/index.ts
+++ skills_expo/assets/styles/index.ts
@@ -1,6 +1,6 @@
 import { StyleSheet, Dimensions } from "react-native";
 
-export const PRIMARY_COLOR = "#7444C0";
+export const PRIMARY_COLOR = "#1E90FF";
 export const SECONDARY_COLOR = "#5636B8";
 export const WHITE = "#FFFFFF";
 export const GRAY = "#757E90";
@@ -10,10 +10,10 @@ export const BLACK = "#000000";
 export const ONLINE_STATUS = "#46A575";
 export const OFFLINE_STATUS = "#D04949";
 
-export const STAR_ACTIONS = "#FFA200";
+export const STAR_ACTIONS = "#5028D7";
 export const LIKE_ACTIONS = "#B644B2";
 export const DISLIKE_ACTIONS = "#363636";
-export const FLASH_ACTIONS = "#5028D7";
+export const FLASH_ACTIONS = "#FFA200";
 
 export const DIMENSION_WIDTH = Dimensions.get("window").width;
 export const DIMENSION_HEIGHT = Dimensions.get("window").height;
